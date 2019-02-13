c72962a177d2569057233f511809ffe368e00d87
D M K <OuroborosDmk@users.noreply.github.com>
2019-02-13 14:54:21 +0800

    Step one
    整合vue-cli

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+<div id="app">
+  <header>
+    <nav>
+      <a href="#">注册/登录</a>
+      <a href="#">日历</a>
+      <a href="#"></a>
+    </nav>
+  </header>
+  <calendar></calendar>
+</div>
+</template>
+
+<script>
+import calendar from './components/calendar'
+export default {
+  name: 'App',
+  components: {
+    calendar: calendar
+  }
+}
+</script>
+
+<style>
+@import "./assets/css/main.css";
+
+</style>
---
+++ src/components/calendar.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <div id="calendarbox">
+            <div id="calendarboxHeader">
+                <a>&lt;</a>
+                <p>2019年2月</p>
+                <a>&gt;</a>
+            </div>
+            <ul id="weekdayHeader">
+                <li v-for="item in weekList" :key="item.value">{{item.title}}</li>
+            </ul>
+            <ul id="weekdayboard">
+                <li></li>
+            </ul>
+        </div>
+        <div id="blackboard">
+
+        </div>
+    </div>
+</template>
+<style>
+@import "../assets/css/component.css";
+</style>
+<script>
+// import { showDate,prevDate,nextDate,subSpace,subDate } from '../js/component.js'
+export default {
+  name: 'calendar',
+  data () {
+    return {
+      weekList: [
+        { title: '日', value: 0 },
+        { title: '一', value: 1 },
+        { title: '二', value: 2 },
+        { title: '三', value: 3 },
+        { title: '四', value: 4 },
+        { title: '五', value: 5 },
+        { title: '六', value: 6 }
+      ]
+    }
+  },
+  mothods: {
+
+  }
+}
+</script>
