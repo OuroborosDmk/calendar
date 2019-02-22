16cca7b525add153f90d97eca742178693f684a5
D M K <OuroborosDmk@users.noreply.github.com>
2019-02-22 11:42:06 +0800

    2.22
    同上，需要解决传参问题

--- src/components/blog.vue
+++ src/components/blog.vue
@@ -1,8 +1,14 @@
 <template>
   <div>
-    <div>
-      <button>新建</button>
-      <button>隐藏</button>
+    <div id="blogheader">
+      <button @click="createBlog">新建</button>
+      <button @click="manage">管理</button>
+      <button @click="sort">排序</button>
+      <input placeholder="请输入要查找内容标题" v-model="searchContent">
+      <button @click="search">查找</button>
+    </div>
+    <div id="bloglist">
+      <table></table>
     </div>
   </div>
 </template>
@@ -14,11 +20,22 @@ export default {
   name: 'blog',
   data () {
     return {
-
+      searchContent: ''
     }
   },
   methods: {
-    
+    createblog:function(){
+
+    },
+    manage:function(){
+
+    },
+    sort:function(){
+
+    },
+    search:function(){
+
+    }
   },
   mounted: function(){
     
